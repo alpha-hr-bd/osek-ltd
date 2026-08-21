55ee1bd546240fd5f1a9ae2aeff1f8b2a58b8de7
AlphaHR <ershadaislam@gmail.com>
2026-08-21 12:24:50 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,34 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  set,
+  push,
+  onValue,
+  update,
+  remove
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDbslFgD45fmc3FrEL0RaHadM_PmfuU0ZM",
+  authDomain: "osek-ltd.firebaseapp.com",
+  databaseURL: "https://osek-ltd-default-rtdb.firebaseio.com",
+  projectId: "osek-ltd",
+  storageBucket: "osek-ltd.firebasestorage.app",
+  messagingSenderId: "499922941529",
+  appId: "1:499922941529:web:925b75878c19e146c4a348",
+  measurementId: "G-9H5X23S9PH"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export {
+  database,
+  ref,
+  set,
+  push,
+  onValue,
+  update,
+  remove
+};
